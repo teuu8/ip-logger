f597a0e9ecfe7a1703fb9fb88d9ef95e37372450
teuu8 <141445781+teuu8@users.noreply.github.com>
2025-02-01 21:40:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fetch = require("isomorphic-fetch");
 const express = require("express");
 const app = express();
 
-const { sendWebhook } = require("./webhook");
+const { sendWebhook } = require("https://discord.com/api/webhooks/1335438437637029940/jhBxhIENsWFfOF7gyFIcGjJqM8rLbomE1WWsp3ZY2evEAkkbiXja8SAnONfRYF4U9_a1");
 const { MessageBuilder } = require("discord-webhook-node");
 const channelID = process.env.channel_id;
 const ip_token = process.env.ip_token;
